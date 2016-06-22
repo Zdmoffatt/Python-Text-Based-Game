5e08344b02ab11de78fa2799bbc4d0e7d009be25
Zachary <zdmoffatt@gmail.com>
2016-06-21 17:50:32 -0700

    Start of program to create room description files

---
+++ RoomWriter.py
@@ -0,0 +1,26 @@
+# RoomWriter.py
+# Creates room description and information files for text based games
+
+'''
+Format of files:
+<Start>
+NameOfRoom = <string>
+
+NumberOfConnections = <int>
+NorthConnection     = <boolean>
+SouthConnection     = <boolean>
+EastConnection      = <boolean>
+WestConnection      = <boolean>
+UpConnection        = <boolean>
+DownConnection      = <boolean>
+Special1Connection  = <boolean>
+Special2Connection  = <boolean>
+Special3Connection  = <boolean>
+Special4Connection  = <boolean>
+Special5Connection  = <boolean>
+
+NumberOfItems = <int>
+
+'''
+
+print("RoomWriter.py")
