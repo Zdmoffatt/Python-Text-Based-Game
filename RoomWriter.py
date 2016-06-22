1adaf78cb60bdf4046ec5675ea7733aaeddf0d43
Zachary <zdmoffatt@gmail.com>
2016-06-21 18:21:09 -0700

    Start of RoomWriter, formatting begins

--- RoomWriter.py
+++ RoomWriter.py
@@ -5,6 +5,9 @@
 Format of files:
 <Start>
 NameOfRoom = <string>
+RoomDescriptionInitial = <string>
+NormalDescriptionDifferent = <boolean>
+RoomDescriptionNormal = <string>
 
 NumberOfConnections = <int>
 NorthConnection     = <boolean>
@@ -21,6 +24,61 @@ Special5Connection  = <boolean>
 
 NumberOfItems = <int>
 
+<ItemNum>
+ItemName = <string>
+ItemDescriptionInRoom = <string>
+ItemDescriptionInInv = <string>
+
+<End>
 '''
 
 print("RoomWriter.py")
+print("What is the room number?")
+roomNum = input()
+roomFile = open(roomNum+'.txt','w')
+roomFile.write("<Start>\n")
+print("Please enter the room name")
+roomName = input()
+roomFile.write("NameOfRoom = "+roomName+"\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+roomFile.close()
